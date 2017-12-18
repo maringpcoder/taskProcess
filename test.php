b880fdc830f9ffdabe4b7bc592cde39334d57cfb
marin <289024114@qq.com>
2017-12-19 00:17:11 +0800

    模拟订阅并发

---
+++ test.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marin
+ * Date: 2017/12/18
+ * Time: 22:04
+ */
+
+ini_set('default_socket_timeout', -1);  //不超时
+try{
+    $redis = new Redis();
+    $redis->pconnect('192.168.1.125', 6379);
+    $redis->subscribe(array('__keyevent@0__:expired'),'tell');
+}catch (RedisException $e){
+    echo 'Error:'.$e->getMessage()."\r\n";
+}
+//$result=$redis->subscribe(array('中央广播电台'), 'tell');
+
+function tell(Redis $instance,$channelName,$message){
+    //error_log(33333,3,'3.log');
+    //var_dump($instance);
+    $timeNow= microtime(true);
+    //echo $timeNow."..... ".$message."    ....message:$channelName  \r\n";
+    call_user_func('lPushList',$message.':'.$timeNow);
+   // $instance->lPush('list_over_time',$message.':'.$timeNow);
+}
+
+function lPushList($listVal)
+{
+    $redis = new Redis();
+    $redis ->connect('192.168.1.125');
+    $redis ->lPush('over_key',$listVal);
+}
