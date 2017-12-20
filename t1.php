5bbe882390fca1fccb1a14dadf34b2436aae82d5
zhengdiao <289024114@qq.com>
2017-12-20 17:43:32 +0800

    异步redis客户端

---
+++ t1.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marin
+ * Date: 2017/12/20
+ * Time: 14:07
+ */
+//$redis = new swoole_redis();
+////echo 3333;
+//$redis->connect('172.16.61.100', 6379, function($c,$r){
+////    var_dump($r);
+//    $c->set("keyer","easyhong",function ($client,$result){
+////        var_dump($client,$result);
+//        $client->close();
+//    });
+//});
+
+namespace App;
+use App\core\AsynRedis;
+
+include_once('./application/bootstrap.php');
+$AsynRedis = AsynRedis::Single('redis_list');
+$AsynRedis ->lpush("my_task",time());
