6f613051386f586b57fa70e23b6d3a53dda3fe54
zhengdiao <289024114@qq.com>
2017-12-25 10:08:58 +0800

    过期处理(删除user.id用户数据)

--- application/module/kvExpiredHandler.php
+++ application/module/kvExpiredHandler.php
@@ -26,7 +26,7 @@ class kvExpiredHandler
     {
         $this->_worker = $worker;
         $this->_redis = RedisCache::getSingleRedis(true, 'redis_list');
-        $this->_arCache = RedisCache::getSingleRedis(false, 'redis_kv_expire');
+        $this->_arCache = RedisCache::getSingleRedis(false, 'redis_ar');
         $this->_config = Config::getConfigArr('panda_server_section');
         swoole_set_process_name($this->getProcessName());
         error_log(date('Y-m-d H:i:s')."\tKVWorker Process {$this->_worker->pid} Start!\n",3,LOG_PATH.'log.txt');
